99ce7c23d54e33b61bc842fa9bc19ff3f19dd925
Christian <164997522+cramseyy@users.noreply.github.com>
2025-01-02 01:12:58 -0500

    game logic done

--- script.js
+++ script.js
@@ -8,11 +8,8 @@ const gameBoard = (function(row1, row2, row3) {
     return { row1, row2, row3 , getBoard}
 })();
 
-const players = (function(player1, player2) {
-    player1 = 'X';
-    player2 = 'O';
-
-    return { player1, player2 }
+const player = (function(name, marker) {
+    return { name, marker }
 })();
 
 function updateBoard(row, col, player) {
@@ -20,10 +17,10 @@ function updateBoard(row, col, player) {
     board[row][col] = player;
 }
 
-function checkWinner() {
+function winCombinations() {
     const board = gameBoard.getBoard();
-    const player1 = players.player1;
-    const player2 = players.player2;
+    const player1 = 'X';
+    const player2 = '0';
 
     // Check rows
     for(let i = 0; i < 3; i++) {
@@ -43,6 +40,7 @@ function checkWinner() {
                 return player1; 
         } else if(board[0][0] === player2 && board[1][1] === player2 && board[2][2] === player2 ||
             board[0][2] === player2 && board[1][1] === player2 && board[2][0] === player2) {
+                game.setGameOver()
                 return player2;
         } else {
             return "Tie!";
@@ -50,9 +48,72 @@ function checkWinner() {
     }
 }
 
+function checkWinner() {
+    const winner = winCombinations();
+    if(winner === 'X'){
+        game.setGameOver();
+        return game.getActivePlayer().name + " wins!";
+    } else if(winner === '0') {
+        game.setGameOver();
+        return game.getActivePlayer().name + " wins!";
+    } else if(winner === "tie"){
+        game.setGameOver();
+        return "Tie!";
+    }
+    return null;
+}
 
-updateBoard(0, 1, players.player1);
-updateBoard(1, 1, players.player1);
-updateBoard(2, 0, players.player1);
+function gameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) {
+    const board = gameBoard.getBoard();
+    let gameOver = false;
+    
+    const players = [
+        {
+            name: playerOneName,
+            mark: 'X'
+        },
+        {
+            name: playerTwoName,
+            mark: '0'
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const checkGameOver = () => gameOver;
+
+    const getActivePlayer = () => activePlayer;
+
+    const setGameOver = () => {
+        gameOver = true;
+    };
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`)
+    }
+
+    return { switchPlayer, checkGameOver, getActivePlayer, setGameOver, board, printNewRound };
+};
+
+const game = gameController();
+
+console.log(game.getActivePlayer().mark);
+// game.switchPlayer();
+
+console.log(game.getActivePlayer().mark);
+updateBoard(0, 0, game.getActivePlayer().mark);
+updateBoard(1, 1, game.getActivePlayer().mark);
+updateBoard(2, 2, game.getActivePlayer().mark);
 console.log(gameBoard.getBoard()); // [ [ '', '', '' ], [ '', '', '' ], [ '', '', '' ] ]
 console.log("Winner: " + checkWinner());
+console.log(game.checkGameOver());
+
+
